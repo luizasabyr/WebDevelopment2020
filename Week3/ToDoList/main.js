f352ad372b25934cc3073be3b28ec82e44cf4fbf
luizasabyr <47174947+luizasabyr@users.noreply.github.com>
2020-03-27 11:27:08 +0600

    Add files via upload

---
+++ Week3/ToDoList/main.js
@@ -0,0 +1,32 @@
+
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("Write something!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+
+    // var span = document.createElement("button");
+    // span.innerHTML = '<i class="new"></i>';
+    // span.classList.add('span');
+    // li.appendChilddChild(span)
+
+
+}
+
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+        console.log(list.children);
+        document.getElementById("myInput").value = this.innerText;
+    }
+}, );
